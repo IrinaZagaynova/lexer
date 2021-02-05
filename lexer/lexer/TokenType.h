0f4560bed4719eb4fe23a765b4e9474c8c552d98
IrinaZagaynova <ayrin.zagaynova@gmail.com>
2021-02-05 22:37:56 +0300

    Добавила Token и TokenType

---
+++ lexer/lexer/TokenType.h
@@ -0,0 +1,25 @@
+#pragma once
+
+enum class TokenType
+{
+	Id,
+	Keyword,
+	Int,
+	Double,
+	Float,
+	Char,
+	String,
+	Array,
+	Addition,
+	Subtraction,
+	Multiplication,
+	Division,
+	Comparison,
+	Assignment,
+	Separator,
+	Bracket,
+	Binary,
+	Octal,
+	Decimal,
+	Error
+};
